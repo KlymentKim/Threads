8488cec7e9a7a77a9a69a2a2bb4eae555f4e76a7
klim86klim <49413026+klim86klim@users.noreply.github.com>
2019-12-07 02:03:46 +0200

    TreeNode, TreeView, Control

---
+++ DZ_14/TreeView_home/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace TreeView_home
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox_SetValue = new System.Windows.Forms.GroupBox();
+            this.Button_ADD = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_Heigth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_Width = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Y = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_X = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.treeView_TS = new System.Windows.Forms.TreeView();
+            this.groupBox_Main = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox_SetValue.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox_SetValue
+            // 
+            this.groupBox_SetValue.Controls.Add(this.Button_ADD);
+            this.groupBox_SetValue.Controls.Add(this.label4);
+            this.groupBox_SetValue.Controls.Add(this.textBox_Heigth);
+            this.groupBox_SetValue.Controls.Add(this.label3);
+            this.groupBox_SetValue.Controls.Add(this.textBox_Width);
+            this.groupBox_SetValue.Controls.Add(this.label2);
+            this.groupBox_SetValue.Controls.Add(this.textBox_Y);
+            this.groupBox_SetValue.Controls.Add(this.label1);
+            this.groupBox_SetValue.Controls.Add(this.textBox_X);
+            this.groupBox_SetValue.Controls.Add(this.comboBox1);
+            this.groupBox_SetValue.Location = new System.Drawing.Point(25, 26);
+            this.groupBox_SetValue.Name = "groupBox_SetValue";
+            this.groupBox_SetValue.Size = new System.Drawing.Size(243, 243);
+            this.groupBox_SetValue.TabIndex = 0;
+            this.groupBox_SetValue.TabStop = false;
+            this.groupBox_SetValue.Text = "Создания элементов";
+            // 
+            // Button_ADD
+            // 
+            this.Button_ADD.Location = new System.Drawing.Point(72, 189);
+            this.Button_ADD.Name = "Button_ADD";
+            this.Button_ADD.Size = new System.Drawing.Size(75, 23);
+            this.Button_ADD.TabIndex = 10;
+            this.Button_ADD.Text = "Добавить";
+            this.Button_ADD.UseVisualStyleBackColor = true;
+            this.Button_ADD.Click += new System.EventHandler(this.button_Add_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(156, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Высота";
+            // 
+            // textBox_Heigth
+            // 
+            this.textBox_Heigth.Location = new System.Drawing.Point(159, 134);
+            this.textBox_Heigth.Name = "textBox_Heigth";
+            this.textBox_Heigth.Size = new System.Drawing.Size(51, 20);
+            this.textBox_Heigth.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ширина";
+            // 
+            // textBox_Width
+            // 
+            this.textBox_Width.Location = new System.Drawing.Point(20, 134);
+            this.textBox_Width.Name = "textBox_Width";
+            this.textBox_Width.Size = new System.Drawing.Size(51, 20);
+            this.textBox_Width.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Y";
+            // 
+            // textBox_Y
+            // 
+            this.textBox_Y.Location = new System.Drawing.Point(159, 75);
+            this.textBox_Y.Name = "textBox_Y";
+            this.textBox_Y.Size = new System.Drawing.Size(51, 20);
+            this.textBox_Y.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X";
+            // 
+            // textBox_X
+            // 
+            this.textBox_X.Location = new System.Drawing.Point(20, 75);
+            this.textBox_X.Name = "textBox_X";
+            this.textBox_X.Size = new System.Drawing.Size(51, 20);
+            this.textBox_X.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(193, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // treeView_TS
+            // 
+            this.treeView_TS.Location = new System.Drawing.Point(25, 275);
+            this.treeView_TS.Name = "treeView_TS";
+            this.treeView_TS.Size = new System.Drawing.Size(243, 276);
+            this.treeView_TS.TabIndex = 1;
+            // 
+            // groupBox_Main
+            // 
+            this.groupBox_Main.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox_Main.Location = new System.Drawing.Point(290, 26);
+            this.groupBox_Main.Name = "groupBox_Main";
+            this.groupBox_Main.Size = new System.Drawing.Size(446, 525);
+            this.groupBox_Main.TabIndex = 2;
+            this.groupBox_Main.TabStop = false;
+            this.groupBox_Main.Text = "Поле";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(748, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Нажмите здесь!";
+            this.notifyIcon1.BalloonTipTitle = "Сообщение";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Хотите открыть? Нажмите дважды";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 596);
+            this.Controls.Add(this.groupBox_Main);
+            this.Controls.Add(this.treeView_TS);
+            this.Controls.Add(this.groupBox_SetValue);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox_SetValue.ResumeLayout(false);
+            this.groupBox_SetValue.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_SetValue;
+        private System.Windows.Forms.Button Button_ADD;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_Heigth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_Width;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Y;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_X;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TreeView treeView_TS;
+        private System.Windows.Forms.GroupBox groupBox_Main;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ DZ_14/TreeView_home/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TreeView_home
+{
+    public partial class Form1 : Form
+    {
+        BindingSource source = new BindingSource();
+        static bool PanelExists { get; set; } = false;
+        static bool GroupBoxExists { get; set; } = false;
+
+
+        public int Xx = 0;
+        public int Yy = 0;
+        public int WidthForm = 0;
+        public int HeigthForm = 0;
+
+        private TextBox txtBox = new TextBox();
+        private Button btnAdd = new Button();
+        private GroupBox grBox = new GroupBox();
+        private Label label = new Label();
+        private Panel panel = new Panel();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            source.DataSource = AddElements();
+            comboBox1.DataSource = source;
+            //TreeNode root = new TreeNode(Button_ADD.Name);
+            //treeView_Title_SubItems.Nodes.Add(root);
+            notifyIcon1.ShowBalloonTip(12);
+        }
+
+        private List<string> AddElements()
+        {
+            List<string> NamesElem = new List<string>();
+            NamesElem.Add("Button");
+            NamesElem.Add("Panel");
+            NamesElem.Add("TextBox");
+            NamesElem.Add("GroupBox");
+            NamesElem.Add("Label");
+
+            return NamesElem;
+        }
+
+        private void button_Add_Click(object sender, EventArgs e)
+        {
+           
+            if (comboBox1.SelectedIndex == -1)
+                return;
+
+            //проверка на добавления только одного элемента Panel ^ GroupBox
+            if ((comboBox1.SelectedItem.ToString() == "Panel" && PanelExists) ||
+                (comboBox1.SelectedItem.ToString() == "GroupBox" && GroupBoxExists))
+                return;
+
+            //проверка групирования в treeView
+            if ((comboBox1.SelectedItem?.ToString() == "Panel" || comboBox1.SelectedItem?.ToString() == "GroupBox") &&
+                (treeView_TS.SelectedNode?.Text == "Panel" || treeView_TS.SelectedNode?.Text == "GroupBox"))
+                return;
+
+            AddValue();
+            Control newControl = null;
+           
+            switch (source[comboBox1.SelectedIndex].ToString())
+            {               
+                case "Button":
+                    CreateButton();
+                    newControl = new Button();
+                  break;
+
+                case "Panel":
+                    PanelExists = true;
+                    panel.BorderStyle = BorderStyle.FixedSingle;
+                    CreatePanel();
+                    newControl = new Panel();
+                    break;
+
+                case "TextBox":
+                    CreateTexBox();
+                    newControl = new TextBox();
+                    break;
+
+                case "GroupBox":
+                     CreateGroupBox();
+                    GroupBoxExists = true;
+                    newControl = new GroupBox();
+                    break;
+
+                case "Label":
+                    CreateLabel();
+                    newControl = new Label();
+                    break;
+                   
+
+            }
+                       
+            newControl.SetBounds(Xx, Yy, WidthForm, HeigthForm);
+
+            TreeNode SelectedNode = treeView_TS.SelectedNode;
+          
+            if (SelectedNode == null || (SelectedNode.Text != "Panel" && SelectedNode.Text != "GroupBox"))
+            {
+                treeView_TS.Nodes.Add(comboBox1.SelectedItem.ToString());
+                groupBox_Main.Controls.Add(newControl);
+            }
+            else
+            {
+                SelectedNode.Nodes.Add(comboBox1.SelectedItem.ToString());
+
+                Control container = groupBox_Main;
+                             
+                    if (SelectedNode.Text == "Panel" && SelectedNode.Text == "GroupBox")
+                    {
+                        container = groupBox_Main.Controls.OfType<Panel>().ToList()[0];
+                        container = groupBox_Main.Controls.OfType<GroupBox>().ToList()[1]; 
+                   }
+                 
+                    container.Controls.Add(newControl);
+              
+            }
+
+        }
+
+        //Parsing
+        private void AddValue()
+        {
+            if (textBox_Heigth.SelectionLength == 0)
+            {
+                int.TryParse(textBox_Heigth.Text, out HeigthForm);
+            }
+            if (textBox_Width.SelectionLength == 0)
+            {
+                int.TryParse(textBox_Width.Text, out WidthForm);
+
+            }
+            if (textBox_X.SelectionLength == 0)
+            {
+                int.TryParse(textBox_X.Text, out Xx);
+
+            }
+            if (textBox_Y.SelectionLength == 0)
+            {
+                int.TryParse(textBox_Y.Text, out Yy);
+            }
+        }
+        //Button
+        private void CreateButton()
+        {           
+            
+            if (comboBox1.SelectedIndex != -1)
+            {
+                btnAdd.BackColor = Color.Gray;
+                btnAdd.Text = "Button";
+                               
+                btnAdd.SetBounds(Xx, Yy, WidthForm, HeigthForm);
+                groupBox_Main.Controls.Add(btnAdd);
+
+            }
+        }
+        //TextBox
+        private void CreateTexBox()
+        {           
+
+            if (comboBox1.SelectedIndex != -1)
+            {
+                txtBox.BackColor = Color.White;
+                txtBox.Text = "TexBox";
+
+                //btnAdd.Location = new Point(Xx, Yy); ;
+                //btnAdd.Size = new Size(WidthForm, HeigthForm);
+                txtBox.SetBounds(Xx, Yy, WidthForm, HeigthForm);
+                groupBox_Main.Controls.Add(txtBox);
+
+            }
+        }
+        //GropBox
+        private void CreateGroupBox()
+        {
+           
+            if (comboBox1.SelectedIndex != -1)
+            {
+                grBox.BackColor = Color.FromArgb(240, 248, 255);
+                grBox.Text = "GroupBox";
+                grBox.SetBounds(Xx, Yy, WidthForm, HeigthForm);
+                groupBox_Main.Controls.Add(grBox);
+            }
+
+        }
+        //Panal
+        private void CreatePanel()
+        {           
+            if (comboBox1.SelectedIndex != -1)
+            {
+               
+                panel.Text = "Panel";
+                panel.BorderStyle = BorderStyle.FixedSingle;
+                panel.SetBounds(Xx, Yy, WidthForm, HeigthForm);
+                groupBox_Main.Controls.Add(panel);
+
+            }
+        }
+
+        private void CreateLabel()
+        {           
+            if (comboBox1.SelectedIndex != -1)
+            {               
+                this.label.Text = "New Label";
+                label.SetBounds(Xx, Yy, WidthForm, HeigthForm);
+                groupBox_Main.Controls.Add(label);
+
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+
+            this.WindowState = FormWindowState.Normal;
+
+        }
+    }
+}
